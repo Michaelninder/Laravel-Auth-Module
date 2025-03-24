1ba9cb61d0c5daa351b2514f522627b046ad92ac
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2025-03-24 19:10:46 +0100

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,14 +1,9 @@
-<head>
-    <title>Login</title>
-</head>
-<body>
-    <h2>Login</h2>
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-        <label>Email:</label>
-        <input type="email" name="email" required>
-        <label>Password:</label>
-        <input type="password" name="password" required>
-        <button type="submit">Login</button>
-    </form>
-</body>
+<h2>Login</h2>
+<form method="POST" action="{{ route('login') }}">
+    @csrf
+    <label>Email:</label>
+    <input type="email" name="email" required>
+    <label>Password:</label>
+    <input type="password" name="password" required>
+    <button type="submit">Login</button>
+</form>
