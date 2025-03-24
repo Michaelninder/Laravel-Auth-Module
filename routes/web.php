fddc0472370a4dec71b5aa1ed7c80ca10b773c9a
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2025-03-24 19:11:15 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth;
 
-Route::get('/login', [Auth\LoginController::class, showLogin];
+Route::get('/login', [Auth\LoginController::class, 'showLogin'];
