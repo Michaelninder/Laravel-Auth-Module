67998bc4eb2e39ba8930c8f304559b861f4abf06
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2025-03-24 19:07:52 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login', [Auth\LoginController::class, showLogin];
