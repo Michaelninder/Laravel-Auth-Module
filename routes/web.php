a79dd18a241ee0ef21101713f476c83a5df6389a
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2025-03-25 11:54:46 +0100

    Dependencies

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LogoutController;
 
-Route::get('/login', [Auth\LoginController::class, 'showLogin'])->name('auth.login');
-Route::get('/register', [Auth\RegisterController::class, 'showRegister'])->name('auth.register');
-Rote::get('/logout', [Auth\LogoutController, 'logout'])->name('auth.logout')->middleware('auth');
+Route::get('/login', [LoginController::class, 'showLogin'])->name('auth.login');
+Route::get('/register', [RegisterController::class, 'showRegister'])->name('auth.register');
+Route::get('/logout', [LogoutController::class, 'logout'])->name('auth.logout')->middleware('auth');
