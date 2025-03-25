65b698494e51b0f71f9a8d1df34e83b4de76101e
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2025-03-25 11:55:23 +0100

    i am stupid

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth;
 
-Route::get('/login', [LoginController::class, 'showLogin'])->name('auth.login');
-Route::get('/register', [RegisterController::class, 'showRegister'])->name('auth.register');
-Route::get('/logout', [LogoutController::class, 'logout'])->name('auth.logout')->middleware('auth');
+Route::get('/login', [Auth\LoginController::class, 'showLogin'])->name('auth.login');
+Route::get('/register', [Auth\RegisterController::class, 'showRegister'])->name('auth.register');
+Route::get('/logout', [Auth\LogoutController::class, 'logout'])->name('auth.logout')->middleware('auth');
