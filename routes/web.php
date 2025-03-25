a664c46544b5872eed7f5f2fbf2a75d24b2c6b2f
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2025-03-25 11:41:10 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use App\Http\Controllers\Auth;
 
 Route::get('/login', [Auth\LoginController::class, 'showLogin'])->name('auth.login');
 Route::get('/register', [Auth\RegisterController::class, 'showRegister'])->name('auth.register');
+Rote::get('/logout', [Auth\LogoutController, 'logout'])->name('auth.logout')->middleware('auth');
