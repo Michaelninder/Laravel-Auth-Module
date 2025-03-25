f759b83514107baa77db6bdf1a17f2e178e90b98
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2025-03-25 12:00:24 +0100

    fix web.php

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use App\Http\Controllers\Auth;
 
 Route::get('/login', [Auth\LoginController::class, 'showLogin'])->name('auth.login');
 Route::get('/register', [Auth\RegisterController::class, 'showRegister'])->name('auth.register');
-Route::post('/register', [RegisterController::class, 'register'])->name('auth.register.post');
+Route::post('/register', [Auth\RegisterController::class, 'register'])->name('auth.register.post');
 Route::get('/logout', [Auth\LogoutController::class, 'logout'])->name('auth.logout')->middleware('auth');
