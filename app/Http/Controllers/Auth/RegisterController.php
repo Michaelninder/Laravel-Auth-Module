dcf4319fe357ecf7b53c8dde51e427300ae73722
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2025-03-25 12:07:22 +0100

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -32,6 +32,6 @@ class RegisterController extends Controller
 
         Auth::login($user);
 
-        return redirect()->route('auth.login');
+        //return redirect()->route('auth.login')->with('success', 'registration successful!');
     }
 }
