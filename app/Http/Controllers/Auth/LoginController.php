ed5826abc126d9c569c3d915d9bbc0d09a29f6fa
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2025-03-25 11:54:23 +0100

    Fixing

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,28 +2,32 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
-  public function showLogin()
-  {
-    return view('auth.login');
-  }
-  
-  public function login(Request $request)
+    public function showLogin()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
     {
         $request->validate([
             'email' => 'required|email',
             'password' => 'required',
         ]);
+
         if (Auth::attempt($request->only('email', 'password'))) {
             return response()->json([
                 'user' => Auth::user(),
                 'message' => 'Successfully logged in',
             ]);
         }
+
         throw ValidationException::withMessages([
             'email' => ['The provided credentials are incorrect.'],
         ]);
